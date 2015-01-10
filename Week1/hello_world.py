880015b3800ce01c0e640ac45d2c53cee2c6cbf3
Todd Doughty <tdoughty1@berkeley.edu>
2015-01-10 04:50:19 -0600

    Added newline at end of html call

--- Week1/hello_world.py
+++ Week1/hello_world.py
@@ -2,11 +2,11 @@ import bottle
 
 @bottle.route('/')
 def home_page():
-    return "Hello World\n"
+    return "<html><title></title><body>Hello World\n</body></html>\n"
 
 @bottle.route('/testpage')
 def test_page():
-    return "This is a test page"
+    return "<html><title></title><body>This is a test page\n</body></html>\n"
 
 bottle.debug(True)
 bottle.run(host='localhost', port=8082)
